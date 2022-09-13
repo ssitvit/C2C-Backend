bff13ef5a1d4249f22ef1dfdc46274a21a871776
ganesh <ganiiscool@gmail.com>
2022-09-13 22:23:59 +0530

    nodemailer config done

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -14,21 +14,44 @@ const sendEmail = async (email, subject, text) => {
           pass: process.env.PASS
         }
     });
-    var mailOptions = {
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+  });
+    let mailOptions = {
         from: 'no@reply.com',
         to: email,
         subject: subject,
         text:"Click on the following link to verify your email address",
         html:`<a href=${text}>Click Here</a>`
       };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+      await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log('Email sent: ' + info.response);
+                resolve(info);
+            }
+        });
+    });
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //       console.log(error);
+    //     } else {
+    //       console.log('Email sent: ' + info.response);
+    //     }
+    //   });
     // console.log("email sent sucessfully");
   } catch (error) {
     console.log("email not sent");
