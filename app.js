46e51c6230dd1916d3730b28f08ed7d57e5edaba
ganesh <ganiiscool@gmail.com>
2022-09-17 00:50:11 +0530

    changed sameSite to SameSite

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 const corsOptions = {
-  origin: "https://code2clone.vercel.app"||"http://localhost:3000/",
+  origin: "https://code2clone.vercel.app"||"http://localhost:3000",
   credentials: true,
 
   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
