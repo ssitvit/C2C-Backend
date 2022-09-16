93d863543d2f97f6c50ab4658b1f9b898fe56ab1
ganesh <ganiiscool@gmail.com>
2022-09-16 15:52:43 +0530

    cors error resolving

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ mongoose
   .catch((error) => console.log(error));
 app.use((req, res, next) => {
   res.header({
-    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": "true",
   });
   next();
