0ad0fa7d42eb5e1d1a68699de721539185f6d8fe
ganesh <ganiiscool@gmail.com>
2022-09-26 22:04:46 +0530

    get all users

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 let corsOptions = {
-  origin: "http://localhost:3000"||"https://code2clone.ieeessitvit.tech",
+  origin: "https://code2clone.ieeessitvit.tech",
   credentials: true,
 
   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
