469e7111c4e88dd26a77355036f6237da3f386c5
ganesh <ganiiscool@gmail.com>
2022-09-28 20:01:29 +0530

    tsting

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 let corsOptions = {
-  origin: "https:/code2clone.ieeessitvit.tech",
+  origin: "https://code2clone.ieeessitvit.tech",
   // origin:process.env.ORIGIN,
   credentials: true,
 
